faccdfee67136e402766383426845a1a71783ec9
rsthornton <rsthornton@gmail.com>
2020-01-10 17:31:37 -0800

    Update and rename dataframe.py to data.py

--- data.py
+++ data.py
@@ -18,53 +18,28 @@ the_lists =  {"gaming" : "Blockchain-gaming", "gurus": "Cryptogurus", "eth" : "E
 
 for slug in the_lists.keys():
     the_lists[slug] = api.list_timeline(screen_name='@shingaithornton', slug = the_lists[slug], owner_screen_name='@shingaithornton', include_rts = 'false', count = 10000)
-   
-   
+    
 # Isolate json of tweepy "status" objects, add them into a list of dictionaries
 
 gaming_tweets, gurus_tweets, eth_tweets, econ_tweets, ai_tweets, btc_tweets, space_tweets, systems_tweets = [],[],[],[],[],[],[],[]
 
-for each_list in the_lists.values():
-    for each_json_tweet in each_list:
-        
-        if each_list == the_lists['gaming']:
-            gaming_tweets.append(each_json_tweet._json)
-            
-        elif each_list == the_lists['gurus']:
-            gurus_tweets.append(each_json_tweet._json)
+def get_tweets(listname, listcollection):
+    for each_list in the_lists.values():
+        for each_json_tweet in each_list:
+            if each_list == the_lists[listname]:
+                listcollection.append(each_json_tweet._json)
 
-        elif each_list == the_lists['eth']:
-            eth_tweets.append(each_json_tweet._json)
-            
-        elif each_list == the_lists['econ']:    
-            econ_tweets.append(each_json_tweet._json)    
-            
-        elif each_list == the_lists['ai']:    
-            ai_tweets.append(each_json_tweet._json)
-        
-        elif each_list == the_lists['btc']:
-           btc_tweets.append(each_json_tweet._json)
-            
-        elif each_list == the_lists['space']:
-            space_tweets.append(each_json_tweet._json)
-        
-        elif each_list == the_lists['systems']:
-            systems_tweets.append(each_json_tweet._json)
-            
+get_tweets('gaming', gaming_tweets), get_tweets('gurus', gurus_tweets), get_tweets('eth', eth_tweets), get_tweets('econ', econ_tweets)
+get_tweets('ai', ai_tweets), get_tweets('btc', btc_tweets), get_tweets('space', space_tweets), get_tweets('systems', systems_tweets) 
+                     
 # Write dictionaries of tweets into text files
                   
 def make_txt(listname, tweets):
     with open('tweet_json_' + str(listname) + '.txt', 'w') as file:
         file.write(json.dumps(tweets, indent=4))
             
-make_txt('gaming', gaming_tweets)
-make_txt('gurus', gurus_tweets)
-make_txt('eth', eth_tweets)
-make_txt('econ', econ_tweets)
-make_txt('ai', ai_tweets)
-make_txt('btc', btc_tweets)
-make_txt('space', space_tweets)
-make_txt('systems', systems_tweets)
+make_txt('gaming', gaming_tweets), make_txt('gurus', gurus_tweets), make_txt('eth', eth_tweets), make_txt('econ', econ_tweets),
+make_txt('ai', ai_tweets), make_txt('btc', btc_tweets), make_txt('space', space_tweets), make_txt('systems', systems_tweets)
     
 # Set up dataframes from text files
 
@@ -94,14 +69,8 @@ def make_list(listname, textfile):
                                  'created_at': str(created_at)
                                 })  
 
-make_list(gaming_list, 'tweet_json_gaming.txt')
-make_list(gurus_list, 'tweet_json_gurus.txt')
-make_list(eth_list, 'tweet_json_eth.txt')
-make_list(econ_list, 'tweet_json_econ.txt')
-make_list(ai_list, 'tweet_json_ai.txt')
-make_list(btc_list, 'tweet_json_btc.txt')
-make_list(space_list, 'tweet_json_space.txt')
-make_list(systems_list, 'tweet_json_systems.txt')
+make_list(gaming_list, 'tweet_json_gaming.txt'), make_list(gurus_list, 'tweet_json_gurus.txt'), make_list(eth_list, 'tweet_json_eth.txt'), make_list(econ_list, 'tweet_json_econ.txt'),
+make_list(ai_list, 'tweet_json_ai.txt'), make_list(btc_list, 'tweet_json_btc.txt'), make_list(space_list, 'tweet_json_space.txt'), make_list(systems_list, 'tweet_json_systems.txt')
    
 # Create dataframes
 
@@ -113,13 +82,5 @@ def make_DF(listname):
 
 eth_df, gaming_df, gurus_df, econ_df, ai_df, btc_df, space_df, systems_df = make_DF(eth_list), make_DF(gaming_list), make_DF(gurus_list), make_DF(econ_list), make_DF(ai_list), make_DF(btc_list), make_DF(space_list), make_DF(systems_list)
 
-
-eth_df.to_excel("ethexcel.xlsx")
-gaming_df.to_excel("gamingexcel.xlsx")
-gurus_df.to_excel("gurusexcel.xlsx")
-econ_df.to_excel("econexcel.xlsx")
-ai_df.to_excel("aiexcel.xlsx")
-btc_df.to_excel("btcxcel.xlsx")
-space_df.to_excel("spaceexcel.xlsx")
-systems_df.to_excel("systemsexcel.xlsx")
-
+eth_df.to_excel("ethexcel.xlsx"), gaming_df.to_excel("gamingexcel.xlsx"), gurus_df.to_excel("gurusexcel.xlsx"), econ_df.to_excel("econexcel.xlsx"),
+ai_df.to_excel("aiexcel.xlsx"), btc_df.to_excel("btcxcel.xlsx"), space_df.to_excel("spaceexcel.xlsx"), systems_df.to_excel("systemsexcel.xlsx")
